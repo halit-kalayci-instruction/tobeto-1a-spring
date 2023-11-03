a43e35912dabe2611e8f8865b6207dd6085fb4bc
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-03 11:17:10 +0300

    person crud

--- src/main/java/com/tobeto/a/spring/intro/PersonsController.java
+++ src/main/java/com/tobeto/a/spring/intro/PersonsController.java
@@ -36,5 +36,34 @@ public class PersonsController {
         personList.add(person);
         return "Eklendi";
     }
+
+    @PutMapping
+    public String update(@RequestBody Person person){
+        // veritabanındaki veriyi bul
+        // VERİTABANINDA İLGİLİ VERİ BULUNMAK ZORUNDA
+        // güncellenmek istenen alanı güncelle
+        Person personToUpdate = personList
+                .stream()
+                .filter(p->p.id == person.id)
+                .findFirst()
+                .orElseThrow(); // throw Exception
+        // böyle bi değer var
+        personToUpdate.name=person.name;
+        personToUpdate.surname=person.surname;
+        return "Güncellendi";
+    }
+
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable int id)
+    {
+        Person personToDelete = personList.stream()
+                .filter(p->p.id==id)
+                .findFirst()
+                .orElseThrow();
+        personList.remove(personToDelete);
+        return "Silindi";
+    }
+
     // Update,Delete
+    //11:05
 }
