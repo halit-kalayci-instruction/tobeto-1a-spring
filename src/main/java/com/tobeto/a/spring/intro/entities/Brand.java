834f35e8d476742d99d70e813693e2482d1fab51
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-17 10:30:36 +0300

    orm

---
+++ src/main/java/com/tobeto/a/spring/intro/entities/Brand.java
@@ -0,0 +1,23 @@
+package com.tobeto.a.spring.intro.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Table(name="brands")
+@Entity
+public class Brand {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @OneToMany(mappedBy = "brand") // DEĞİŞKEN İSMİ
+    private List<Car> cars;
+}
+// one-to-many ilişkilerde list olan değişken
+// @onetomany
+// direkt class olan değişken @manytoone
---
+++ src/main/java/com/tobeto/a/spring/intro/entities/Car.java
@@ -0,0 +1,37 @@
+package com.tobeto.a.spring.intro.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+// ORM => Object Relation Mapping
+@Table(name = "cars")
+@Entity
+public class Car
+{
+    @Id
+    @Column(name = "id") // name = kolon_adı
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; // classın içindeki alanın adı
+
+    @Column(name="model_year")
+    private int modelYear;
+
+    @Column(name="model_name")
+    private String modelName;
+
+    @Column(name="daily_price")
+    private BigDecimal dailyPrice;
+
+    @Column(name="color")
+    private String color;
+
+    @Column(name="status")
+    private String status;
+
+    // TODO: İlişkisel Brand tablosunun alanlarını eklemek.
+
+    @ManyToOne
+    @JoinColumn(name="brand_id")
+    private Brand brand;
+}
