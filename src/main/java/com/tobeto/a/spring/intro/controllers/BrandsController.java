dad598bed4d040ce46657695bdab1e8fc3fdd05f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-20 09:51:27 +0300

    brands controller

--- src/main/java/com/tobeto/a/spring/intro/controllers/BrandsController.java
+++ src/main/java/com/tobeto/a/spring/intro/controllers/BrandsController.java
@@ -1,4 +1,31 @@
 package com.tobeto.a.spring.intro.controllers;
 
+import com.tobeto.a.spring.intro.entities.Brand;
+import com.tobeto.a.spring.intro.repositories.BrandRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/brands")
 public class BrandsController {
+    // final => sadece ctor'da ilgili değerin ataması yapılabileceğini söyler.
+    private final BrandRepository brandRepository;
+    public BrandsController(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+    // Dependency Injection
+
+    // Spring IoC => DI Container
+
+
+    @GetMapping
+    public List<Brand> getAll() {
+        // findAll => hazır bir JPArepository fonk.
+        List<Brand> brands = brandRepository.findAll();
+        return brands;
+    }
 }
+// http://localhost:8080/api/brands GET
