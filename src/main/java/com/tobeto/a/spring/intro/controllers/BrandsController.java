9b11ed2f5efa38ac80a3c93fe33c27881d50692b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-20 11:01:15 +0300

    jpa repository crud

--- src/main/java/com/tobeto/a/spring/intro/controllers/BrandsController.java
+++ src/main/java/com/tobeto/a/spring/intro/controllers/BrandsController.java
@@ -2,9 +2,7 @@ package com.tobeto.a.spring.intro.controllers;
 
 import com.tobeto.a.spring.intro.entities.Brand;
 import com.tobeto.a.spring.intro.repositories.BrandRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,29 @@ public class BrandsController {
         List<Brand> brands = brandRepository.findAll();
         return brands;
     }
+
+    @GetMapping("{id}")
+    public Brand getById(@PathVariable int id){
+        // Optional => Opsiyonel olarak Brand
+        // verilen id ile bir veri varsa onu dön, yoksa exception fırlat
+        return brandRepository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public void add(@RequestBody Brand brand){
+        brandRepository.save(brand);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
+        Brand brandToDelete = brandRepository.findById(id).orElseThrow();
+        // exception fırlaması = bu satırın çalışmaması
+        // bu satıra geldiyse, bu id ile bir veri vardır.
+        brandRepository.delete(brandToDelete);
+    }
+
+    // update
 }
 // http://localhost:8080/api/brands GET
+
+// JSON Infinite Recursion
