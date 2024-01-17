e1a9dd13b9261d824457894525fb51264856302b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-17 11:18:14 +0300

    register

--- src/main/java/com/tobeto/a/spring/intro/core/configurations/ApplicationConfiguration.java
+++ src/main/java/com/tobeto/a/spring/intro/core/configurations/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.tobeto.a.spring.intro.core.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,7 +18,10 @@ public class ApplicationConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(x -> x.anyRequest().permitAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x -> x.anyRequest().permitAll())
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
